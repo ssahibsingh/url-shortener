779aabc5e9c17b502644c9d9a7d0d6444f19d6b5
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-22 13:14:58 +0530

    Added Custom Feature

--- app.js
+++ app.js
@@ -26,27 +26,33 @@ var longURL;
 var error = "Server Error!! Please Try Again Later.";
 
 app.get('/', (req, res) => {
-    res.render('index', { error: "", shortURL: "", shortID: "" });
+    res.render('index', { error: "", newURL: {} });
 })
 
 app.post('/', (req, res) => {
-    const shortID = nanoid(10);
+    var shortID = nanoid(11);
+
+    if (req.body.customAlias != "") {
+        shortID = req.body.customAlias;
+    }
 
     longURL = req.body.longURL;
+    var shortURL = process.env.BASE_URL + "/" + shortID;
 
-    if (validURL.isUri(longURL)) {
-        const newURL = new URL({
-            longURL: longURL,
-            shortURL: process.env.BASE_URL + "/" + shortID,
-            shortID: shortID
-        })
+    const newURL = new URL({
+        longURL: longURL,
+        shortURL: shortURL,
+        shortID: shortID
+    })
+
+    if (validURL.isUri(longURL) && validURL.isUri(shortURL)) {
         URL.findOne({ longURL: longURL }, function (err, result) {
             if (!err) {
                 if (result == null) {
                     newURL.save(function (erro) {
                         if (erro) {
                             // console.log(erro);
-                            res.render('index', { error: error, shortID: "", shortURL: "" });
+                            res.render('index', { error: error, newURL: {} });
                         }
                         else {
                             displayShortURL(req, res);
@@ -61,20 +67,38 @@ app.post('/', (req, res) => {
             }
             else {
                 // console.log("Error Occured in Finding");
-                res.render('index', { error: error, shortURL: "", shortID: "" });
+                res.render('index', { error: error, newURL: {} });
             }
         })
     }
-    else {
-        error = "Invalid URL";
-        res.render('index', { error: error, shortURL: "", shortID: "" });
+    else if (!validURL.isUri(longURL) && !validURL.isUri(shortURL)) {
+        error = "Invalid Long URL and Custom Alias!";
+        res.render('index', { error: error, newURL: {} });
     }
+    else if (!validURL.isUri(longURL)) {
+        error = "Invalid Long URL!";
+        res.render('index', { error: error, newURL: {} });
+    }
+    else if (!validURL.isUri(shortURL)) {
+        error = "Invalid Custom Alias!";
+        res.render('index', { error: error, newURL: {} });
+    }
+
+
+    // if (validURL.isUri(longURL)) {
+
+
+    // }
+    // else {
+    //     error = "Invalid URL";
+    //     res.render('index', { error: error, shortURL: "", shortID: "" });
+    // }
 })
 
 function displayShortURL(req, res) {
     URL.findOne({ longURL: longURL }, function (err, result) {
         if (!err) {
-            res.render('index', { shortURL: result.shortURL, error: "", shortID: result.shortID });
+            res.render('index', { error: "", newURL: result });
         }
     })
 }
